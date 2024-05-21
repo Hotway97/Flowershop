5a5b9894bdeb8421f83e7f4337f94ae5a1a716ba
Vladimir Dorofeev <necromemeser@gmail.com>
2024-05-21 15:46:16 +0300

    tryna deploy

--- src/main/java/com/example/flowers/repositories/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.flowers.repositories;
-
-import com.example.flowers.models.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-}
--- src/main/java/com/example/flowers/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.flowers.repositories;
-
-import com.example.flowers.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
